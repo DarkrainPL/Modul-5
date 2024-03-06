8d18b19fdda690fba91ab5b830a4001c2f8f4969
Karol <karolkaraszewski@gmail.com>
2024-03-06 18:43:00 +0100

    Added task5 not working yet

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,18 +17,32 @@ public class Main {
         //task2.readFromFile("files/ala.txt");
 
         // Task3
-        GeometricFigure rectangle = new Rectangle(3,4);
-        System.out.println(rectangle.calculateArea());
+        //GeometricFigure rectangle = new Rectangle(3,4);
+        //System.out.println(rectangle.calculateArea());
 
-        GeometricFigure triangle = new Triangle(5,4);
-        System.out.println(triangle.calculateArea());
+        //GeometricFigure triangle = new Triangle(5,4);
+        //System.out.println(triangle.calculateArea());
 
-        GeometricFigure hexagon = new Hexagon(6);
-        System.out.println(hexagon.calculateArea());
+        //GeometricFigure hexagon = new Hexagon(6);
+        //System.out.println(hexagon.calculateArea());
 
         // Task4
 
-        Square square = new Square(5,5);
-        System.out.println(square.calculateDiameter(5));
+        //Square square = new Square(5,5);
+        //System.out.println(square.calculateDiameter(5));
+
+        // Task5
+
+        FileOperationTasks task5a = new FileOperationTasks();
+        FileOperationTasks task5b = new FileOperationTasks();
+
+        task5a.readFromFile("files/ala.txt");
+        task5b.readFromFile("files/ala 2.txt");
+
+        if (task5a.equals(task5b)) {
+            System.out.println("The content of both files is the same!");
+        } else {
+            System.out.println("The content of both files is different!");
+        }
     }
 }
