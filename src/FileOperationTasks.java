0618b24ed5a80c084454c24196e3e6dd49c2e484
Karol <karolkaraszewski@gmail.com>
2024-03-06 17:00:34 +0100

    Added task2 without bonus

--- src/FileOperationTasks.java
+++ src/FileOperationTasks.java
@@ -1,13 +1,62 @@
-import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
 public class FileOperationTasks {
+
     // Task 1
     public void printDirectory() {
-        File directoryPath = new File("d:\\JAVATEST");
+        File directoryPath = new File("files");
         String[] contents = directoryPath.list();
         System.out.println("List of files and directories in the specified directory:");
         for (int i = 0; i < contents.length; i++) {
             System.out.println(contents[i]);
         }
+    }
+
+    // Task 2
+    public boolean writeUserInputToFile() {
+        String path = "files";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tell me the filename:");
+        String filename = sc.nextLine();
+        System.out.println("Tell me the text you want to save in file:");
+        String textToSave = sc.nextLine();
+
+
+        File file = new File(path, filename);
+        file.deleteOnExit();
+        try (FileWriter writer = new FileWriter(file);
+             BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
+
+            file.createNewFile();
+            bufferedWriter.write(textToSave);
+            bufferedWriter.flush();
+            return true;
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            return false;
+        }
+    }
+
+    public void readFromFile(String fileName) {
+
+        String text;
+        File file = new File(fileName);
+        StringBuilder builder = new StringBuilder();
+        try (FileReader reader = new FileReader(fileName);
+             BufferedReader bufferedReader = new BufferedReader(reader))
+        {
+            text = bufferedReader.readLine();
+            while (text != null) {
+                builder.append(text + " ");
+                text = bufferedReader.readLine();
+            }
+            System.out.println("In file " + fileName + " there is this text:");
+            System.out.println(builder);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+
+        }
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,10 +6,13 @@ public class Main {
         // FileOperationTasks task1 = new FileOperationTasks();
         // task1.printDirectory();
 
-
-
-
-
+        // Task2
+        FileOperationTasks task2 = new FileOperationTasks();
+        if (task2.writeUserInputToFile()) {
+            System.out.println("The operation of adding text to file was successful!");
+        } else {
+            System.out.println("The operation of adding text to file was unsuccessful!");
         }
-
+        task2.readFromFile("files/ala.txt");
     }
+}
