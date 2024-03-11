e816630554db2af77b9d271c691b0be35c9f32b2
Karol <karolkaraszewski@gmail.com>
2024-03-11 13:46:57 +0100

    Updated task 2 part 2

--- src/FileOperationTasks.java
+++ src/FileOperationTasks.java
@@ -36,79 +36,90 @@ public class FileOperationTasks {
             try (FileWriter writer = new FileWriter(file);
                  BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
                 file.createNewFile();
-                bufferedWriter.write(textToSave);
-                bufferedWriter.flush();
-                return true;
+                String[] stringToArray = textToSave.split("\\s+");
+                for (int i = 0; i <= stringToArray.length; i++) {
+
+                    if ((i = 1) % 4 != 0) {
+                        bufferedWriter.write(stringToArray[i] + " ");
+                    } else {
+                        bufferedWriter.write(stringToArray[i]);
+                        bufferedWriter.newLine();
+                    }
+
+                    bufferedWriter.flush();
+                    return true;
+
+                }
             } catch (IOException ioException) {
                 ioException.printStackTrace();
                 return false;
             }
         }
+        return false;
     }
+        public void readFromFile (String fileName){ // string.split, StringBuilder, 4 slowa znak nowej linii
 
-    public void readFromFile(String fileName) { // string.split, StringBuilder, 4 slowa znak nowej linii
-
-        String text;
-        File file = new File(fileName);
-        if (file.exists()) {
-            StringBuilder builder = new StringBuilder();
-            try (FileReader reader = new FileReader(file);
-                 BufferedReader bufferedReader = new BufferedReader(reader)) {
-                text = bufferedReader.readLine();
-                while (text != null) {
-                    builder.append(text + " ");
+            String text;
+            File file = new File(fileName);
+            if (file.exists()) {
+                StringBuilder builder = new StringBuilder();
+                try (FileReader reader = new FileReader(file);
+                     BufferedReader bufferedReader = new BufferedReader(reader)) {
                     text = bufferedReader.readLine();
-                }
-                System.out.println("In file " + fileName + " there is this text:");
-                System.out.println(builder);
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-
-            }
-        } else {
-            System.out.println("File doesn't exist!");
-        }
-    }
-
-    public void checkIfTwoFilesHaveSameContent(String fileName1, String fileName2) {
-
-        String text1;
-        String text2;
-        File file1 = new File(fileName1);
-        File file2 = new File(fileName2);
-        if (file1.exists() && file2.exists()) {
-
-            try (FileReader reader1 = new FileReader(file1);
-                 BufferedReader bufferedReader1 = new BufferedReader(reader1);
-                 FileReader reader2 = new FileReader(file2);
-                 BufferedReader bufferedReader2 = new BufferedReader(reader2)) {
-
-                text1 = bufferedReader1.readLine();
-                text2 = bufferedReader2.readLine();
-                boolean answer = true;
-
-                while (text1 != null && text2 != null) {
-                    if (text1.equals(text2)) {
-                        answer = true;
-                        text1 = bufferedReader1.readLine();
-                        text2 = bufferedReader2.readLine();
-                    } else {
-                        answer = false;
-                        text1 = bufferedReader1.readLine();
-                        text2 = bufferedReader2.readLine();
+                    while (text != null) {
+                        builder.append(text + " ");
+                        text = bufferedReader.readLine();
                     }
-                }
-                if (answer == true) {
-                    System.out.println("These files have the same content!");
-                } else {
-                    System.out.println("These files have different content!");
-                }
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
+                    System.out.println("In file " + fileName + " there is this text:");
+                    System.out.println(builder);
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
 
+                }
+            } else {
+                System.out.println("File doesn't exist!");
             }
-        } else {
-            System.out.println("File doesn't exist!");
+        }
+
+        public void checkIfTwoFilesHaveSameContent (String fileName1, String fileName2){
+
+            String text1;
+            String text2;
+            File file1 = new File(fileName1);
+            File file2 = new File(fileName2);
+            if (file1.exists() && file2.exists()) {
+
+                try (FileReader reader1 = new FileReader(file1);
+                     BufferedReader bufferedReader1 = new BufferedReader(reader1);
+                     FileReader reader2 = new FileReader(file2);
+                     BufferedReader bufferedReader2 = new BufferedReader(reader2)) {
+
+                    text1 = bufferedReader1.readLine();
+                    text2 = bufferedReader2.readLine();
+                    boolean answer = true;
+
+                    while (text1 != null && text2 != null) {
+                        if (text1.equals(text2)) {
+                            answer = true;
+                            text1 = bufferedReader1.readLine();
+                            text2 = bufferedReader2.readLine();
+                        } else {
+                            answer = false;
+                            text1 = bufferedReader1.readLine();
+                            text2 = bufferedReader2.readLine();
+                        }
+                    }
+                    if (answer == true) {
+                        System.out.println("These files have the same content!");
+                    } else {
+                        System.out.println("These files have different content!");
+                    }
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+
+                }
+            } else {
+                System.out.println("File doesn't exist!");
+            }
         }
     }
-}
